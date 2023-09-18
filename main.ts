4f39c2db9f3a1dfbba6bdecdc080959c320e5506
Ivan Riazanov <omega19961@gmail.com>
2023-09-18 19:10:10 +0600

    added i18n and sqlite db

--- main.ts
+++ main.ts
@@ -1,17 +1,51 @@
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
+import TelegrafI18n from 'telegraf-i18n'
+import { Database } from "bun:sqlite";
+
+const db = new Database("mydb.sqlite", { create: true });
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const i18n = new TelegrafI18n({
+    defaultLanguage: 'en',
+    allowMissing: true, // Default true
+    directory: './locales'
+  })
 
+bot.use(i18n.middleware())
 
 bot.start( async (ctx) => {
-    await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello! This bot can help you save your money`)
-    await ctx.telegram.sendMessage(ctx.message.chat.id, `Привет! Этот бот поможет вам сохранить деньги`)
+
+    await ctx.reply("Choose language! Выберите язык", {
+        reply_markup: {
+            inline_keyboard: [
+                [ { text: "English", callback_data: "locale_en" }, { text: "Русский", callback_data: "locale_ru" } ],
+            ]
+        }
+    });
 })
+
+bot.action('locale_en',async (ctx)=>{
+    console.log(ctx.chat.id)
+    db.transaction(() => {  });
+    await ctx.reply('hahalol english')
+})
+
+bot.action('locale_ru',async (ctx)=>{
+    await ctx.reply('хахалол русский')
+})
+
+
+
 
 bot.launch()
 
+const stopBot = ()=>{
+    bot.stop('SIGINT');
+    db.close();
+}
+
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
-process.once('SIGKILL', () => bot.stop('SIGKILL'))
+process.once('SIGINT', stopBot)
+process.once('SIGTERM', stopBot)
+process.once('SIGKILL', stopBot)
